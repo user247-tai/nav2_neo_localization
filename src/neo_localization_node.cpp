0ba26a332529de21f255bb4d9762e1897fcb203b
tai <nexoncolors@gmail.com>
2025-10-24 16:30:59 +0700

    Remove personal usage topic name

--- src/neo_localization_node.cpp
+++ src/neo_localization_node.cpp
@@ -233,7 +233,7 @@ void NeoLocalizationNode::initPubSub()
   m_sub_scan_topic = create_subscription<sensor_msgs::msg::LaserScan>(m_scan_topic, rclcpp::SensorDataQoS(), std::bind(&NeoLocalizationNode::scan_callback, this, _1));
   m_sub_map_topic = create_subscription<nav_msgs::msg::OccupancyGrid>("/map", rclcpp::QoS(rclcpp::KeepLast(1)).transient_local().reliable(), std::bind(&NeoLocalizationNode::map_callback, this, _1));
   m_sub_pose_estimate = create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(m_initial_pose, 1, std::bind(&NeoLocalizationNode::pose_callback, this, _1));
-  m_sub_only_use_odom = create_subscription<std_msgs::msg::Bool>("/global_costmap/binary_state", rclcpp::QoS(10).transient_local().reliable(), std::bind(&NeoLocalizationNode::use_odom_callback, this, _1));
+  m_sub_only_use_odom = create_subscription<std_msgs::msg::Bool>("/only_use_odom", rclcpp::QoS(10).transient_local().reliable(), std::bind(&NeoLocalizationNode::use_odom_callback, this, _1));
 
   // Init Publishers
   m_pub_map_tile = create_publisher<nav_msgs::msg::OccupancyGrid>(m_map_tile, 1);
